5ac36881c064b5d7df265f76176e7f39475ed49e
Peter Zsilak <peter.zsilak@gmail.com>
2026-07-06 16:10:45 +0200

    Add an authentication test.

---
+++ src/tests/auth.setup.ts
@@ -0,0 +1,12 @@
+import { expect } from '@playwright/test';
+
+import { loadCredential } from '@/config/env-loader';
+import { test } from '@/fixture/page-init.fixture';
+
+test.describe('A user can', () => {
+  test('authenticate', async ({ page, loginPage, inventoryPage }) => {
+    await page.goto('/');
+    await loginPage.authenticate(loadCredential());
+    await expect(inventoryPage.items.root, 'Inventory should show items after login').not.toHaveCount(0);
+  });
+});
